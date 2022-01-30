344b572d3d2403b07f45ba3120a82efdf8d8b686
simonescevaroli <98689485+simonescevaroli@users.noreply.github.com>
2022-01-30 15:53:41 +0100

    Add files via upload

---
+++ Utils/Debug/LcdDriver/Crystalfontz128x128_ST7735.d
@@ -0,0 +1,174 @@
+# FIXED
+
+LcdDriver/Crystalfontz128x128_ST7735.obj: ../LcdDriver/Crystalfontz128x128_ST7735.c
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/grlib/grlib.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/assert.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: ../LcdDriver/Crystalfontz128x128_ST7735.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/driverlib.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/adc14.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp_compatibility.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r_classic.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/core_cm4.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_version.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_compiler.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_ccs.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/mpu_armv7.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/system_msp432p401r.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/aes256.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/comp_e.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/cpu.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/crc32.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/cs.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/dma.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/interrupt.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/eusci.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/fpu.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/gpio.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/i2c.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/mpu.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/pcm.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/pmap.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/pss.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/ref_a.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/reset.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/rom.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/rom_map.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/rtc_c.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/spi.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/systick.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/timer32.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/timer_a.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/uart.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/wdt_a.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/sysctl.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/flash.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/grlib/grlib.h
+LcdDriver/Crystalfontz128x128_ST7735.obj: ../LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.h
+
+../LcdDriver/Crystalfontz128x128_ST7735.c:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/grlib/grlib.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/assert.h:
+
+../LcdDriver/Crystalfontz128x128_ST7735.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/driverlib.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/adc14.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp_compatibility.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r_classic.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/core_cm4.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_version.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_compiler.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_ccs.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/mpu_armv7.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/system_msp432p401r.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/aes256.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/comp_e.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/cpu.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/crc32.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/cs.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/dma.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/interrupt.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/eusci.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/fpu.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/gpio.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/i2c.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/mpu.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/pcm.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/pmap.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/pss.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/ref_a.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/reset.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/rom.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/rom_map.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/rtc_c.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/spi.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/systick.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/timer32.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/timer_a.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/uart.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/wdt_a.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/sysctl.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/flash.h:
+
+C:/Users/Simone/Downloads/simplelink_msp432p4_sdk_3_40_01_02/source/ti/grlib/grlib.h:
+
+../LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.h:
+
